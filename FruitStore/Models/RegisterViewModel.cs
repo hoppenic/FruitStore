dd9845c9727fad7a4731e526a18f6bf6846b8a0b
hoppenic <hoppenic@gmail.com>
2018-06-12 07:41:43 -0500

    update

---
+++ FruitStore/Models/RegisterViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace FruitStore.Models
+{
+    public class RegisterViewModel
+    {
+        [Required]
+        public string UserName { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        public string PhoneNumber { get; set; }
+
+        [Required]
+        public string FirstName { get; set; }
+
+        [Required]
+        public string LastName { get; set; }
+
+
+
+
+    }
+}
