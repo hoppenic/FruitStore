c1b4e2b8e58de056c76662a3259f93893c7d24a3
hoppenic <hoppenic@gmail.com>
2018-06-12 07:25:42 -0500

    update

---
+++ FruitStore/Controllers/AccountController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using FruitStore.Models;
+using Microsoft.AspNetCore.Identity;
+
+namespace FruitStore.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        //Responds on GET /Account/Register
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        //Responds on POST /Account/Register
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Register(string username, string password)
+        {
+
+            return Ok();
+
+        }
+
+
+    }
+}
