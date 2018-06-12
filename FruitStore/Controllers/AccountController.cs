560315a96c88e279352b0588ea5fb4e84bfc2c9b
hoppenic <hoppenic@gmail.com>
2018-06-12 09:17:07 -0500

    update

--- FruitStore/Controllers/AccountController.cs
+++ FruitStore/Controllers/AccountController.cs
@@ -10,6 +10,17 @@ namespace FruitStore.Controllers
 {
     public class AccountController : Controller
     {
+
+        SignInManager<IdentityUser> _signInManager;
+
+        //using Microsoft.aspnet.core.identity
+        //this is a constructor
+        public AccountController(SignInManager<IdentityUser> signInManager)
+        {
+            _signInManager = signInManager; 
+        }
+
+        
         public IActionResult Index()
         {
             return View();
@@ -17,7 +28,6 @@ namespace FruitStore.Controllers
 
 
         //Responds on GET /Account/Register
-
         public IActionResult Register()
         {
             return View();
@@ -30,10 +40,74 @@ namespace FruitStore.Controllers
         {
             if (ModelState.IsValid)
             {
-                //TODO create an account and log this user in
-                return RedirectToAction("Index", "Home");
+                IdentityUser newUser = new IdentityUser(model.UserName);
+
+                IdentityResult creationResult = _signInManager.UserManager.CreateAsync(newUser).Result;
+                if (creationResult.Succeeded)
+                {
+                    IdentityResult passwordResult = _signInManager.UserManager.AddPasswordAsync(newUser, model.Password).Result;
+                    if (passwordResult.Succeeded)
+                    {
+                        _signInManager.SignInAsync(newUser, false).Wait();
+                        return RedirectToAction("Index", "Home");
+                    }
+                    else
+                    {
+                        foreach (var error in passwordResult.Errors)
+                        {
+                            ModelState.AddModelError(error.Code, error.Description);
+                        }
+                    }
+                }
+                else
+                {
+                    foreach (var error in creationResult.Errors)
+                    {
+                        ModelState.AddModelError(error.Code, error.Description);
+                    }
+                }
             }
 
+            return View();
+        }
+
+        public IActionResult SignOut()
+        {
+            _signInManager.SignOutAsync().Wait();
+            return RedirectToAction("Index", "Home");
+        }
+
+        //responds on GET
+        public IActionResult SignIn()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult SignIn(SignInViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                IdentityUser existingUser = _signInManager.UserManager.FindByNameAsync(model.UserName).Result;
+                if(existingUser != null)
+                {
+                    Microsoft.AspNetCore.Identity.SignInResult passwordResult = _signInManager.CheckPasswordSignInAsync(existingUser, model.Password, false).Result;
+                    if (passwordResult.Succeeded)
+                    {
+                        _signInManager.SignInAsync(existingUser, false).Wait();
+                        return RedirectToAction("Index", "Home");
+                    }
+                    else
+                    {
+                        ModelState.AddModelError("PasswordIncorrect", "Username or password is incorrect");
+                    }
+                }
+                else
+                {
+                    ModelState.AddModelError("UserDoesNotExist", "Username or password is incorrect");
+                }
+            }
             return View();
 
         }
